2e487ee56fe8c84a097fe283ed449177a6647bf3
margaridalx <margarida.mvls@gmail.com>
2024-03-09 20:00:47 +0000

    implementação de opções (senha, cifra, decifrar)

--- main.c
+++ main.c
@@ -1,15 +1,52 @@
 #include "cifras.h"
 #include <stdio.h>
+#include <unistd.h>
 
 #define DEFAULTPASSWORD "Programacao2024"
 #define MAXLENGTH 100
 
-int main(void) {
+int main(int argc, char *argv[]) {
   char texto[MAXLENGTH];
   char cifra[MAXLENGTH];
-  printf("Introduza senha: ");
-  fgets(texto, MAXLENGTH, stdin);
+  char *senha = NULL;
 
-  cifra_de_vigenere(texto, DEFAULTPASSWORD, cifra);
-  printf(" O texto %s foi cifrado para: %s ", texto, cifra);
+  int ch;
+  while ((ch = getopt(argc, argv, "s:c:d:")) != -1) {
+    switch (ch) {
+    case 's':
+      senha = optarg;
+      break;
+    case 'c': {
+      switch (*optarg) {
+      case '1':
+        puts("Cifra de César");
+        break;
+      case '2':
+        puts("Cifra de Vigenére");
+        break;
+      default:
+        puts("Cifra Inválida");
+      }
+      break;
+    }
+    case 'd': {
+      switch (*optarg) {
+      case '1':
+        puts("Decifrar Cifra de César");
+        break;
+      case '2':
+        puts("Decifrar Cifra de Vigenére");
+        break;
+      default:
+        puts("Cifra Inválida");
+      }
+      break;
+    }
+    default:
+      puts("Use -h para saber opções");
+    }
+  }
+  printf("A senha é: %s", senha);
+  // cifra_de_vigenere(texto, DEFAULTPASSWORD, cifra);
+  // printf(" O texto %s foi cifrado para: %s ", texto, cifra);
 }
